ab5e3cd9cf20aca50e766dc77535ce77b9b4c329
Sagar Salokhe <sagar.salokhe@zillasecurity.com>
2024-12-09 13:14:57 +0530

    refactor Dockerfile and index.ts for improved Playwright setup and server launch

--- playwright/index.ts
+++ playwright/index.ts
@@ -4,15 +4,14 @@ import https from 'https';
 import url from 'url';
 import fs from 'fs';
 
-async function launchChromeWithRemoteDebugging() {
-  console.log('Launching Chrome with remote debugging...');
+async function launchChromeServer() {
+  console.log('Launching Chrome Server...');
   const browser = await chromium.launchServer({
     headless: false,
     port: 9222,
     wsPath: '/devtools/browser/my-browser-id', // Custom WebSocket path
-    args: ['--remote-debugging-port=9224'] // Enable remote debugging on port 9222
   });
-  console.log('Chrome browser launched with remote debugging enabled');
+  console.log('Chrome browser launched as server');
 
   const wsEndpoint = browser.wsEndpoint();
   console.log("WebSocket Endpoint: ", wsEndpoint);
@@ -25,8 +24,8 @@ async function launchChromeWithRemoteDebugging() {
 
   // Paths to your self-signed certificate and private key
   const options = {
-    key: fs.readFileSync('server.key'), // Private key
-    cert: fs.readFileSync('server.cert') // Self-signed certificate
+    key: fs.readFileSync('/home/neko/server.key'), // Private key
+    cert: fs.readFileSync('/home/neko/server.cert') // Self-signed certificate
   };
 
   // Create an HTTP proxy server
@@ -83,31 +82,9 @@ async function launchChromeWithRemoteDebugging() {
   // Start the server
   server.listen(9223, async () => {
     console.log('Proxy server listening on https://127.0.0.1:9223');
-    
-    //Fetch the WebSocket URL from the proxy server
-    //const response = await fetch('https://127.0.0.1:8080/json');
-    //const data = await response.json();
-    //let wsUrl = data.webSocketDebuggerUrl;
-
-    //Set environment variable to ignore self-signed certificate errors
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-
-    const wsUrl = "wss://127.0.0.1:9223/devtools/browser/my-browser-id?token=YOUR_SECRET_TOKEN"; // Custom WebSocket path
-
-    console.log('WebSocket URL:', wsUrl);
-
-    // Launch a new Playwright Chrome instance to connect to the WebSocket URL
-    const p = await chromium.launch();
-    p.con
-    const proxyBrowser = await chromium.connectOverCDP(wsUrl, {});
-    proxyBrowser.newBrowserCDPSession
-    const proxyContext = await proxyBrowser.newContext();
-    const proxyPage = await proxyContext.newPage();
-    await proxyPage.goto('https://dev.zsec.io'); // Replace with the desired URL
-    // console.log('Playwright Chrome instance connected to the proxy server on port 9223');
   });
 
   return { browser };
 }
 
-launchChromeWithRemoteDebugging().catch(console.error);
+launchChromeServer().catch(console.error);
